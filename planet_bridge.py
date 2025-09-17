f6381430f2bc684960de9525b0dedceb1b91dab4
deepakkfin <deepakkfin@gmail.com>
2025-09-17 15:01:13 +0530

    Update planet_bridge.py
    Update the port section for webservice

--- planet_bridge.py
+++ planet_bridge.py
@@ -70,4 +70,7 @@ def planet_default(planet_name):
     return planet_json(planet_name)
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env var
+    app.run(host="0.0.0.0", port=port, debug=False)
+
